ec14fc5129518ce98988b1ceeaa483595827a140
arofatak <arofatabdillah@gmail.com>
2026-06-14 18:45:57 +0700

    fix typo dan gantri script pada toggle tema

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,31 @@
-const btnMode = document.getElementById('btnMode');
+// const btnMode = document.getElementById('btnMode');
+//
+// // Menambahkan fungsi klik pada tombol untuk mengubah tema warna sederhana
+// btnMode.addEventListener('click', function() {
+//     const currentBg = document.body.style.backgroundColor;
+//
+//     if (currentBg === 'rgb(44, 62, 80)' || currentBg === '#2c3e50') {
+//         // Kembalikan ke Mode Terang
+//         document.body.style.backgroundColor = '#f8f9fa';
+//         document.body.style.color = '#333';
+//         btnMode.textContent = 'Ubah Tema Halaman';
+//     } else {
+//         // Ubah ke Mode Gelap Ringan
+//         document.body.style.backgroundColor = '#2c3e50';
+//         document.body.style.color = '#ffffff';
+//         btnMode.textContent = 'Kembalikan Tema';
+//     }
+// });
 
-// Menambahkan fungsi klik pada tombol untuk mengubah tema warna sederhana
-btnMode.addEventListener('click', function() {
-    const currentBg = document.body.style.backgroundColor;
+const btnMode = document.getElementById("btnMode");
 
-    if (currentBg === 'rgb(44, 62, 80)' || currentBg === '#2c3e50') {
-        // Kembalikan ke Mode Terang
-        document.body.style.backgroundColor = '#f8f9fa';
-        document.body.style.color = '#333';
-        btnMode.textContent = 'Ubah Tema Halaman';
-    } else {
-        // Ubah ke Mode Gelap Ringan
-        document.body.style.backgroundColor = '#2c3e50';
-        document.body.style.color = '#ffffff';
-        btnMode.textContent = 'Kembalikan Tema';
+btnMode.addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        btnMode.textContext = "Kembalikan Tema";
+    }
+    else {
+        btnMode.textContext = "Ubah Tema Halaman";
     }
 });
